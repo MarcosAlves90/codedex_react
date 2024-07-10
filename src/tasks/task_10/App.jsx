cb1a1015d2df5aba430f10083d61b275e619d8b2
Marcos Alves <MarcosAlves25@proton.me>
2024-07-10 15:01:15 -0300

    Add tasks 6 to 10.

---
+++ src/tasks/task_10/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header.js";
+import Reviews from './Reviews.js'
+import Footer from "./Footer.js";
+// Import component here 💖
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Reviews />
+            {/* Write code here 💖 */}
+            <Footer />
+        </div>
+    );
+}
---
+++ src/tasks/task_8.jsx
@@ -0,0 +1,12 @@
+export default function Task8() {
+
+    const barcelonaImage = <img src={"https://bit.ly/3HNBfm9"}  alt="Barcelona" />;
+    const tokyoImage = <img src={"https://bit.ly/3uqeiSM"}  alt="Tokyo" />;
+    const ohioImage = <img src={"https://bit.ly/3whR81p"}  alt="Ohio" />;
+
+    const imageGallery = [<li>{barcelonaImage}</li>, <li>{tokyoImage}</li>, <li>{ohioImage}</li>]
+
+    return <ul>
+        {imageGallery}
+    </ul>;
+}
